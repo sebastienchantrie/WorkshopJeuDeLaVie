91e0909f787066e742b42a415dd6a2580ad51f23
Sébastien Chantrie <sebastien.chantrie@gmail.com>
2019-10-18 10:51:16 +0200

    'ajoutlastfichier'

---
+++ script.js
@@ -0,0 +1,18 @@
+// Global Var //
+
+let btnclick = document.getElementById('createtab');
+let btnread = document.getElementById('btnread');
+let btnpause = document.getElementById('btnpause');
+let comptaffiche = document.getElementById('nbgen');
+let bodytab = document.getElementById('gameoflife');
+let rulesbtn = document.getElementById('btnrules');
+let btnback = document.getElementById('btnback');
+let rules = document.getElementById('rules');
+let preset1 = document.getElementById('btnpreset1');
+
+// <-------------------------------------------------------------------------------> //
+
+// Code here...
+
+
+
